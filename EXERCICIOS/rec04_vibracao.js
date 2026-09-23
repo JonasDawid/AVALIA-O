e9504986575bba738327b38f6494fd5cc16908fb
JonasDawid <jonasdawinho@gmail.com>
2026-09-23 15:06:26 -0300

    EXERCÍCIO 4

---
+++ EXERCICIOS/rec04_vibracao.js
@@ -0,0 +1,10 @@
+const entrada = require ('readline-sync');
+
+const valor_vibracao = entrada.questionInt (" Qual o valor da vibracao?:");
+
+if (valor_vibracao <= 3 ){
+    console.log(" situação ESTÁVEL")
+} else if ( valor_vibracao <= 4){
+    console.log("ATENÇÃO!!!")
+} else { (valor_vibracao >= 6) 
+    console.log("SITUAÇÃO CRITICA !!!")}
