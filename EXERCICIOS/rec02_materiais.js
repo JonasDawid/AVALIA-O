67c714c8b2ad3c7026c41968cd3ff91907aeb95e
JonasDawid <jonasdawinho@gmail.com>
2026-09-23 14:24:39 -0300

    exercício 2

---
+++ EXERCICIOS/rec02_materiais.js
@@ -0,0 +1,13 @@
+const entrada = require ('readline-sync');
+
+const nome_peca = entrada.question("nome da peca:");
+const quantidade_peca = entrada.questionFloat (" quantidade adquirida:");
+const preco_peca = entrada.questionFloat("preço unitario da peca:");
+
+const valor_final = quantidade_peca * preco_peca
+
+console.log("---- RELATÓRIO DA COMPRA ----");
+console.log(`Nome da peca: ${nome_peca}`);
+console.log(`Quantidade adquirida: ${quantidade_peca}`);
+console.log(`Preço Unitario da peça: ${preco_peca}`);
+console.log (`Valor a pagar: ${valor_final}`);
